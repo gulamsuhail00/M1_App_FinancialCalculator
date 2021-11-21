eca2031a82dab68fc1847dc76b33ab46fbe5952f
alrichroshan <alrichroshan@gmail.com>
2021-11-21 11:33:12 +0530

    inc update

---
+++ 3_Implementation/inc/feature.h
@@ -0,0 +1,28 @@
+/**
+ * @file feature.h
+ * @author Alrich Roshan
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-17
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef FEATURE_H_
+#define FEATURE_H_
+
+
+int cagr();
+int discountcalculator();
+int ppf();
+int emi();
+int rd();
+int fd();
+int lumpsum();
+int sip();
+
+
+
+#define MACRO -99
+#endif
